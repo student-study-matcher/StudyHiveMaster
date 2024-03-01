50805ea0d42479a5ccc4b99a394219a2ede8a93e
billywolfe321 <127870679+billywolfe321@users.noreply.github.com>
2024-03-01 12:06:09 +0000

    Update Notifications.dart

--- lib/Notifications.dart
+++ lib/Notifications.dart
@@ -13,213 +13,62 @@ class _NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffffffff),
       appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        automaticallyImplyLeading: false,
         backgroundColor: Color(0xffae32ff),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.zero,
-        ),
+        elevation: 0,
         title: Text(
           "Notifications",
           style: TextStyle(
             fontWeight: FontWeight.w400,
-            fontStyle: FontStyle.normal,
             fontSize: 20,
-            color: Color(0xfffdfdfd),
+            color: Colors.white,
           ),
         ),
+        centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
         ),
       ),
       body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
           children: [
-            Card(
-              margin: EdgeInsets.fromLTRB(16, 30, 16, 0),
-              color: Color(0xffffffff),
-              shadowColor: Color(0xffd5d2d2),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
-                side: BorderSide(color: Color(0x4d9e9e9e), width: 1),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      "Notification Settings",
-                      textAlign: TextAlign.start,
-                      overflow: TextOverflow.clip,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 16,
-                        color: Color(0xff000000),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                      child: SwitchListTile(
-                        value: pushNotification,
-                        title: Text(
-                          "Push Notification",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 14,
-                            color: Color(0xff000000),
-                          ),
-                          textAlign: TextAlign.start,
-                        ),
-                        subtitle: Text(
-                          "Receive weekly push notification",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 12,
-                            color: Color(0xff737070),
-                          ),
-                          textAlign: TextAlign.start,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        ),
-                        onChanged: (value) {
-                          setState(() {
-                            pushNotification = value;
-                          });
-                        },
-                        tileColor: Color(0x00ffffff),
-                        activeColor: Color(0xff3a57e8),
-                        activeTrackColor: Color(0x3f3a57e8),
-                        controlAffinity: ListTileControlAffinity.trailing,
-                        dense: false,
-                        inactiveThumbColor: Color(0xff9e9e9e),
-                        inactiveTrackColor: Color(0xffe0e0e0),
-                        contentPadding: EdgeInsets.all(0),
-                        selected: false,
-                        selectedTileColor: Color(0x42000000),
-                      ),
-                    ),
-                    Divider(
-                      color: Color(0xff808080),
-                      height: 16,
-                      thickness: 0.3,
-                      indent: 0,
-                      endIndent: 0,
-                    ),
-                    SwitchListTile(
-                      value: chatNotification,
-                      title: Text(
-                        "Chat Notification",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                        textAlign: TextAlign.start,
-                      ),
-                      subtitle: Text(
-                        "Receive chat notification",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 12,
-                          color: Color(0xff737070),
-                        ),
-                        textAlign: TextAlign.start,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          chatNotification = value;
-                        });
-                      },
-                      tileColor: Color(0x00ffffff),
-                      activeColor: Color(0xff3a57e8),
-                      activeTrackColor: Color(0x3f3a57e8),
-                      controlAffinity: ListTileControlAffinity.trailing,
-                      dense: false,
-                      inactiveThumbColor: Color(0xff9e9e9e),
-                      inactiveTrackColor: Color(0xffe0e0e0),
-                      contentPadding: EdgeInsets.all(0),
-                      selected: false,
-                      selectedTileColor: Color(0x42000000),
-                    ),
-                    Divider(
-                      color: Color(0xff808080),
-                      height: 16,
-                      thickness: 0.3,
-                      indent: 0,
-                      endIndent: 0,
-                    ),
-                    SwitchListTile(
-                      value: emailNotification,
-                      title: Text(
-                        "Email Notification",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
-                        ),
-                        textAlign: TextAlign.start,
-                      ),
-                      subtitle: Text(
-                        "Receive email notification",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 12,
-                          color: Color(0xff737070),
-                        ),
-                        textAlign: TextAlign.start,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          emailNotification = value;
-                        });
-                      },
-                      tileColor: Color(0x00ffffff),
-                      activeColor: Color(0xff3a57e8),
-                      activeTrackColor: Color(0x3f3a57e8),
-                      controlAffinity: ListTileControlAffinity.trailing,
-                      dense: false,
-                      inactiveThumbColor: Color(0xff9e9e9e),
-                      inactiveTrackColor: Color(0xffe0e0e0),
-                      contentPadding: EdgeInsets.all(0),
-                      selected: false,
-                      selectedTileColor: Color(0x42000000),
-                    ),
-                  ],
-                ),
-              ),
+            buildNotificationSetting(
+              "Push Notification",
+              pushNotification,
+                  (value) => setState(() => pushNotification = value),
             ),
-            SizedBox(
-              height: 16,
-              width: 16,
+            buildNotificationSetting(
+              "Chat Notification",
+              chatNotification,
+                  (value) => setState(() => chatNotification = value),
+            ),
+            buildNotificationSetting(
+              "Email Notification",
+              emailNotification,
+                  (value) => setState(() => emailNotification = value),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildNotificationSetting(String title, bool value, ValueChanged<bool> onChanged) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListTile(
+          title: Text(title, style: TextStyle(fontSize: 16)),
+          subtitle: Text("Receive $title", style: TextStyle(fontSize: 12)),
+          trailing: Switch(
+            value: value,
+            onChanged: onChanged,
+            activeColor: Color(0xff3a57e8),
+          ),
         ),
       ),
     );

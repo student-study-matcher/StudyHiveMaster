3377d58819683975e90dff20ac5e6ca8046bcf0e
billywolfe321 <127870679+billywolfe321@users.noreply.github.com>
2024-03-01 12:08:24 +0000

    Update UserProfile.dart
    firebase logic. UI might've changed slightly, so pls make it look better

--- lib/UserProfile.dart
+++ lib/UserProfile.dart
@@ -1,33 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+//import 'FriendsPopup.dart';
 import 'index.dart';
-
-
 class UserProfile extends StatefulWidget {
   @override
   _UserProfileState createState() => _UserProfileState();
 }
-
 class _UserProfileState extends State<UserProfile> {
-  List<String> friends = [
-    "Friend 1", "Friend 2", "Friend 3", "Friend 4", "Friend 5",
-    "Friend 6", "Friend 7", "Friend 8", "Friend 9", "Friend 10"
-  ];
-  bool isDropdownOpen = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final databaseReference = FirebaseDatabase.instance.ref();
 
+  String firstName = '';
+  String lastName = '';
+  String username = '';
+  String bio = '';
+  String university = '';
+  String course = '';
+  List<Map<String, String>> friendsDetails = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+  Future<void> fetchUserData() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      final userSnapshot = await databaseReference.child('Users/${user.uid}').get();
+      if (userSnapshot.exists) {
+        final userData = userSnapshot.value as Map<dynamic, dynamic>;
+        setState(() {
+          firstName = userData['firstName'] ?? '';
+          lastName = userData['lastName'] ?? '';
+          username = userData['username'] ?? '';
+          bio = userData['bio'] ?? '';
+          university = userData['university'] ?? '';
+          course = userData['course'] ?? '';
+
+          if (userData.containsKey('friends')) {
+            fetchFriendsDetails(userData['friends'] as Map<dynamic, dynamic>);
+          }
+        });
+      }
+    }
+  }
+  Future<void> fetchFriendsDetails(Map<dynamic, dynamic> friendsIds) async {
+    List<Map<String, String>> fetchedFriendsDetails = [];
+    for (String friendId in friendsIds.keys) {
+      final friendSnapshot = await databaseReference.child('Users/$friendId').get();
+      if (friendSnapshot.exists) {
+        final friendData = friendSnapshot.value as Map<dynamic, dynamic>;
+        fetchedFriendsDetails.add({
+          'id': friendId,
+          'username': friendData['username'] ?? 'Unknown',
+        });
+      }
+    }
+    setState(() {
+      friendsDetails = fetchedFriendsDetails;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xffffffff),
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Color(0xffad32fe),
         title: GestureDetector(
-          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen())),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
           child: Row(
             children: [
               Image.asset('assets/logo.png', width: 28),
               SizedBox(width: 28),
-              Text("Study Hive", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 16, color: Colors.white)),
+              Text("Study Hive", style: TextStyle(fontWeight: FontWeight.w400, fontStyle: FontStyle.normal, fontSize: 16, color: Color(0xffffffff))),
             ],
           ),
         ),
@@ -42,108 +92,83 @@ class _UserProfileState extends State<UserProfile> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          bottomNavItem(Icons.article, "Forums"),
-          bottomNavItem(Icons.message, "Messages"),
-          bottomNavItem(Icons.account_box, "Profile"),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.article), label: "Forums"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Messages"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "Profile"),
         ],
         onTap: (int index) {
           if (index == 0) {
-            Navigator.push(context, MaterialPageRoute(builder: (_) => Forums()));
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Forums()));
           } else if (index == 1) {
-            // Handle Messages
           } else if (index == 2) {
-            Navigator.push(context, MaterialPageRoute(builder: (_) => UserProfile()));
           }
         },
         backgroundColor: Color(0xffae32ff),
-        elevation: 8,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Color(0xffffffff),
+        unselectedItemColor: Color(0xffffffff),
       ),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            CircleAvatar(radius: 60, backgroundImage: AssetImage("assets/profilePicture1.png")),
+            SizedBox(height: 10),
+            Text("$firstName $lastName", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            Text(username, style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SizedBox(height: 20),
+                ProfileInfoBox(title: course, subtitle: "Subject"),
                 GestureDetector(
                   onTap: () {
-                    // select photo
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FriendsPopup(friendsDetails: friendsDetails),
+                      ),
+                    );
                   },
-                  child: CircleAvatar(
-                    radius: 60,
-                    backgroundImage: AssetImage("assets/profilePicture1.png"),
-                  ),
+                  child: ProfileInfoBox(title: "${friendsDetails.length} Friends", subtitle: ""),
                 ),
-                SizedBox(height: 10),
-                Text("Angelina", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Text("Hudhra", style: TextStyle(fontSize: 18)),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ProfileInfoBox(title: "Big Data"),
-                    ProfileFriendsButton(
-                      onPressed: () {
-                        setState(() {
-                          isDropdownOpen = !isDropdownOpen;
-                        });
-                      },
-                    ),
-                    ProfileInfoBox(title: "University of Portsmouth"),
-                  ],
-                ),
-                SizedBox(height: 20),
-                ProfileBioBox(
-                  initialBio: "I like football",
-                  onBioChanged: (newBio) {
-                    //  bio
-                  },
-                ),
+                ProfileInfoBox(title: university, subtitle: "University"),
               ],
             ),
-          ),
-          if (isDropdownOpen)
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: FriendsPopup(
-                friends: friends,
-                onFriendSelected: (friend) {
-                  setState(() {
-                    isDropdownOpen = false;
-                  });
-                },
-                onClose: () => setState(() {
-                  isDropdownOpen = false;
-                }),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(bio),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-        ],
+          ],
+        ),
       ),
-    );
-  }
-
-  BottomNavigationBarItem bottomNavItem(IconData icon, String label) {
-    return BottomNavigationBarItem(
-      icon: Icon(icon),
-      label: label,
     );
   }
 }
-
 class ProfileInfoBox extends StatelessWidget {
   final String title;
-
-  ProfileInfoBox({required this.title});
+  final String subtitle;
+  ProfileInfoBox({required this.title, required this.subtitle});
 
   @override
   Widget build(BuildContext context) {
@@ -155,100 +180,18 @@ class ProfileInfoBox extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Text(title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
           SizedBox(height: 5),
-          Text("", style: TextStyle(fontSize: 14)),
-        ],
-      ),
-    );
-  }
-}
-
-class ProfileFriendsButton extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  ProfileFriendsButton({required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: onPressed,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                Text("Friends", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ProfileBioBox extends StatefulWidget {
-  final String initialBio;
-  final void Function(String newBio) onBioChanged;
-
-  ProfileBioBox({required this.initialBio, required this.onBioChanged});
-
-  @override
-  _ProfileBioBoxState createState() => _ProfileBioBoxState();
-}
-
-class _ProfileBioBoxState extends State<ProfileBioBox> {
-  late String _bio;
-  late TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _bio = widget.initialBio;
-    _controller = TextEditingController(text: _bio);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text("Bio", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-              Spacer(),
-              IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  widget.onBioChanged(_bio);
-                },
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          TextFormField(
-            controller: _controller,
-            onChanged: (value) {
-              setState(() {
-                _bio = value;
-              });
-            },
-            decoration: InputDecoration(
-              hintText: "Type your bio here...",
-              border: InputBorder.none,
+          Text(
+            subtitle,
+            style: TextStyle(
+              fontSize: 14,
             ),
           ),
         ],

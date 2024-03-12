9aa15915b1427ffd77217c953dfaf3d44546895d
billywolfe321 <127870679+billywolfe321@users.noreply.github.com>
2024-03-12 12:56:06 +0000

    Create Privacy.dart

---
+++ lib/Privacy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'index.dart';
+
+class Privacy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rgrrgqg'),
+      ),
+      body: Center(
+        child: Text('Reporqgrqggqere'),
+        // Add your reporting form or functionality here
+      ),
+    );
+  }
+}

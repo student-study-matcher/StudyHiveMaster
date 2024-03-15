9eacf7f5092626e147a6e5cf7cf0c1c6947eb846
AngelinaHdr <147830324+AngelinaHdr@users.noreply.github.com>
2024-03-15 13:11:53 +0000

    Update ChatPage.dart

--- lib/ChatPage.dart
+++ lib/ChatPage.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
-import 'index.dart';
+import 'ChatBottomSheet.dart';
+import 'ChatSample.dart';
+import 'EditChat.dart'; 
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(70.0),
         child: Padding(
-          padding: EdgeInsets.only(top: 5),
+          padding: EdgeInsets.only(top:5),
           child: AppBar(
             leadingWidth: 30,
-            title: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.asset(
-                    "images/avatar.jpg",
-                    height: 45,
-                    width: 45,
-                  ),
+            title: Row(children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.asset(
+                  "images/avatar.jpg",
+                  height: 45,
+                  width:45,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Text(
-                    "Mark",
-                    style: TextStyle(color: Color(0xFF113953)),
-                  ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left:10),
+                child: Text(
+                  "Maths",
+                  style: TextStyle(color: Color(0xFF113953)),
                 ),
-              ],
-            ),
+              ),
+            ],),
             actions: [
               Padding(
-                padding: EdgeInsets.only(right: 25),
-                child: Icon(
-                  Icons.call,
+                padding: EdgeInsets.only(right:25),
+                child: Icon(Icons.call,
                   color: Color(0xFF113953),
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: Icon(
-                  Icons.more_vert,
-                  color: Color(0xFF113953),
+                padding: EdgeInsets.only(right:10),
+                child: IconButton( // Changed from Icon to IconButton
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Color(0xFF113953),
+                  ),
+                  onPressed: () {
+                    
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EditChat()),
+                    );
+                  },
                 ),
               ),
             ],
@@ -50,10 +58,10 @@ class ChatPage extends StatelessWidget {
         ),
       ),
       body: ListView(
-        padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 80),
-        children: [
-          ChatSample(),
-        ],
+          padding: EdgeInsets.only(top: 20, left: 20, right:20, bottom: 80),
+          children: [
+            ChatSample()
+          ]
       ),
       bottomSheet: ChatBottomSheet(),
     );

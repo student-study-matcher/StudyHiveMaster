60b0b7690abe1c665a90405e7f02500eebca6cb9
billywolfe321 <127870679+billywolfe321@users.noreply.github.com>
2024-04-23 12:31:43 +0100

    Update AddForum.dart

--- lib/AddForum.dart
+++ lib/AddForum.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:login_and_registration/OpenDrawer.dart';
 
 class AddForum extends StatefulWidget {
   @override
@@ -12,54 +16,126 @@ class _AddForumState extends State<AddForum> {
   final TextEditingController _contentController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final DatabaseReference _databaseReference = FirebaseDatabase.instance.ref();
-  String selectedSubject = 'General'; 
+  List<String> subjects = [];
+  String? selectedSubject;
+  Uint8List? _fileBytes;
+  String? _fileName;
 
   @override
-  void dispose() {
-    _titleController.dispose();
-    _contentController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    fetchSubjects();
+  }
+
+  void fetchSubjects() async {
+    DatabaseReference ref = FirebaseDatabase.instance.ref('Subjects');
+    DatabaseEvent event = await ref.once();
+    if (event.snapshot.exists) {
+      List<String> loadedSubjects = [];
+      event.snapshot.children.forEach((DataSnapshot snapshot) {
+        loadedSubjects.add(snapshot.value.toString());
+      });
+      setState(() {
+        subjects = loadedSubjects;
+        if (subjects.isNotEmpty) selectedSubject = subjects[0];
+      });
+    }
+  }
+
+  Future<void> pickFile() async {
+    final result = await FilePicker.platform.pickFiles(
+        allowMultiple: false,
+        type: FileType.custom,
+        allowedExtensions: ['pdf', 'png', 'jpeg', 'doc', 'docx'],
+        withData: true
+    );
+
+    if (result != null) {
+      String? pickedFileName = result.files.single.name;
+      List<String> allowedExtensions = ['pdf', 'png', 'jpeg', 'doc', 'docx'];
+      String fileExtension = pickedFileName.split('.').last;
+
+      if (!allowedExtensions.contains(fileExtension.toLowerCase())) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Invalid file type. Allowed types: PDF, PNG, JPEG, DOC, DOCX'))
+        );
+        return;
+      }
+
+      if (result.files.single.size <= 10485760) {
+        setState(() {
+          _fileBytes = result.files.single.bytes;
+          _fileName = result.files.single.name;
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('File too large (max size: 10MB)'))
+        );
+      }
+    }
+  }
+
+
+  Future<String?> uploadFile() async {
+    if (_fileBytes != null) {
+      try {
+        String userId = _auth.currentUser?.uid ?? '';
+        String filePath = 'private/$userId/$_fileName';
+        final ref = FirebaseStorage.instance.ref().child(filePath);
+        final result = await ref.putData(_fileBytes!);
+        return await result.ref.getDownloadURL();
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to upload file: $e')));
+        return null;
+      }
+    }
+    return null;
   }
 
   void _submitForum() async {
-    final String title = _titleController.text.trim();
-    final String content = _contentController.text.trim();
-    final String userId = _auth.currentUser?.uid ?? '';
-
-    if (title.isNotEmpty && content.isNotEmpty && userId.isNotEmpty) {
-      final forum = {
-        'title': title,
-        'content': content,
-        'authorID': userId,
-        'subject': selectedSubject,
-        'timestamp': ServerValue.timestamp,
-        'responses': {},
-      };
-      
-      await _databaseReference.child('Forums').push().set(forum);
-      
-      _titleController.clear();
-      _contentController.clear();
-
-      
-      Navigator.pop(context);
-    } else {
+    if (_titleController.text.isEmpty || _contentController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Title and content cannot be empty')),
+          SnackBar(content: Text('Title and content cannot be empty'))
       );
+      return;
     }
+
+    String? fileUrl = await uploadFile();
+
+    String userId = _auth.currentUser?.uid ?? '';
+    Map<String, dynamic> forum = {
+      'title': _titleController.text.trim(),
+      'content': _contentController.text.trim(),
+      'subject': selectedSubject,
+      'authorID': userId,
+      'timestamp': ServerValue.timestamp,
+      'responses': {},
+      'fileUrl': fileUrl,
+    };
+
+    await _databaseReference.child('Forums').push().set(forum);
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffffffff),
       appBar: AppBar(
-        backgroundColor: Color(0xffad32fe),
-        title: Text("Add Forum"),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            Text('Add Forum'),
+            SizedBox(width: 48),
+          ],
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      drawer: OpenDrawer(),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -82,20 +158,40 @@ class _AddForumState extends State<AddForum> {
             SizedBox(height: 16),
             DropdownButton<String>(
               value: selectedSubject,
+              isExpanded: true,
               onChanged: (String? newValue) {
                 setState(() {
                   selectedSubject = newValue!;
                 });
               },
-              items: <String>['General', 'Computing', 'Maths', 'Science']
-                  .map<DropdownMenuItem<String>>((String value) {
+              items: subjects.map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(value),
                 );
               }).toList(),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 20),
+            _fileName == null
+                ? ElevatedButton(
+              onPressed: pickFile,
+              child: Text('Attach File'),
+            )
+                : Column(
+              children: [
+                Text('Attached File: $_fileName'),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _fileBytes = null;
+                      _fileName = null;
+                    });
+                  },
+                  child: Text('Remove File'),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: _submitForum,
               child: Text('Submit Forum'),

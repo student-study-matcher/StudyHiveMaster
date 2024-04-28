790641b361e92ccd6fd5903cf4e9871c34cab33d
AngelinaHdr <147830324+AngelinaHdr@users.noreply.github.com>
2024-04-28 23:22:39 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'index.dart';
+import 'Login.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
-
-
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyD9Rmsgi-hOz6eZUTcVbb7QUFtzPQcXiJk",
-      appId: "1:795494178260:web:bd07547862dd0eec870a41",
-      messagingSenderId: "795494178260",
-      projectId: "study-hive-9a0e6",
-      databaseURL: "https://study-hive-9a0e6-default-rtdb.europe-west1.firebasedatabase.app",
-    ),
+      options: FirebaseOptions(
+          apiKey: "AIzaSyD9Rmsgi-hOz6eZUTcVbb7QUFtzPQcXiJk",
+          appId: "1:795494178260:web:bd07547862dd0eec870a41",
+          messagingSenderId: "795494178260",
+          projectId: "study-hive-9a0e6",
+          databaseURL: "https://study-hive-9a0e6-default-rtdb.europe-west1.firebasedatabase.app",
+          storageBucket: "study-hive-9a0e6.appspot.com"
+      )
   );
   runApp(MyApp());
 }
@@ -23,11 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: "/",
-      routes: {
-        "/": (context) => HomeScreen(),
-        "chatPage": (context) => ChatPage(),
-      },
+      home: Login(),
     );
   }
 }

0dcdfd9fb9d5af47d887d7f9ba69e975d09477ae
AngelinaHdr <147830324+AngelinaHdr@users.noreply.github.com>
2024-04-28 23:26:37 +0100

    Update UserProfile.dart

--- lib/UserProfile.dart
+++ lib/UserProfile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'index.dart';
-
+import 'OpenDrawer.dart';
 
 class UserProfile extends StatefulWidget {
   @override
@@ -8,12 +10,79 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
-  List<String> friends = [
-    "Friend 1", "Friend 2", "Friend 3", "Friend 4", "Friend 5",
-    "Friend 6", "Friend 7", "Friend 8", "Friend 9", "Friend 10"
-  ];
-  bool isDropdownOpen = false;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
 
+  String firstName = '';
+  String lastName = '';
+  String username = '';
+  String bio = '';
+  String university = '';
+  int profilePic = 0;
+  String course = '';
+  List<Map<String, String>> friendsDetails = [];
+  bool isEditingBio = false;
+  bool isEditingName = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        final userSnapshot = await databaseReference.child('Users/${user.uid}').get();
+        if (userSnapshot.exists) {
+          final userData = userSnapshot.value as Map<dynamic, dynamic>;
+          setState(() {
+            firstName = userData['firstName'] ?? '';
+            lastName = userData['lastName'] ?? '';
+            username = userData['username'] ?? '';
+            bio = userData['bio'] ?? '';
+            university = userData['university'] ?? '';
+            course = userData['course'] ?? '';
+            profilePic = userData['profilePic'] != null ? userData['profilePic'] : 0;
+
+            if (userData.containsKey('friends')) {
+              fetchFriendsDetails(userData['friends'] as Map<dynamic, dynamic>);
+            }
+          });
+        }
+      }
+    } catch (error) {
+      print("Error fetching user data: $error");
+    }
+  }
+
+  Future<void> fetchFriendsDetails(Map<dynamic, dynamic> friendsIds) async {
+    List<Map<String, String>> fetchedFriendsDetails = [];
+    for (String friendId in friendsIds.keys) {
+      final friendSnapshot = await databaseReference.child('Users/$friendId').get();
+      if (friendSnapshot.exists) {
+        final friendData = friendSnapshot.value as Map<dynamic, dynamic>;
+        fetchedFriendsDetails.add({
+          'id': friendId,
+          'username': friendData['username'] ?? 'Unknown',
+        });
+      }
+    }
+    setState(() {
+      friendsDetails = fetchedFriendsDetails;
+    });
+  }
+
+  void updateNameInDatabase() {
+    final user = _auth.currentUser;
+    if (user != null) {
+      databaseReference.child('Users/${user.uid}').update({
+        'firstName': firstName,
+        'lastName': lastName,
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,191 +90,153 @@ class _UserProfileState extends State<UserProfile> {
       backgroundColor: Colors.white,
       drawer: OpenDrawer(),
       appBar: CustomAppBar(),
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    // select photo
-                  },
-                  child: CircleAvatar(
-                    radius: 60,
-                    backgroundImage: AssetImage("assets/profilePicture1.png"),
-                  ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Align(
+    alignment: Alignment.topCenter,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(2.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width * 0.7,
+                decoration: BoxDecoration(
+                  color: Color(0xFFF5F5F5),
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                SizedBox(height: 10),
-                Text("Angelina", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                Text("Hudhra", style: TextStyle(fontSize: 18)),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    ProfileInfoBox(title: "Big Data"),
-                    ProfileFriendsButton(
-                      onPressed: () {
-                        setState(() {
-                          isDropdownOpen = !isDropdownOpen;
-                        });
-                      },
+                    Text(
+                      'Your Profile',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    ProfileInfoBox(title: "University of Portsmouth"),
+                    SizedBox(height: 20),
+                    CircleAvatar(radius: 60, backgroundImage: AssetImage(getProfilePicturePath(profilePic))),
+                    SizedBox(height: 10),
+                    !isEditingName
+                        ? Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("$firstName $lastName", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                        IconButton(icon: Icon(Icons.edit, size: 20), onPressed: () => setState(() => isEditingName = true)),
+                      ],
+                    )
+                        : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: TextEditingController(text: "$firstName $lastName"),
+                            onChanged: (value) {
+                              List<String> names = value.split(' ');
+                              if (names.isNotEmpty) {
+                                firstName = names.first;
+                                lastName = names.length > 1 ? names.sublist(1).join(' ') : '';
+                              }
+                            },
+                            decoration: InputDecoration(hintText: "Enter your full name", border: InputBorder.none),
+                            autofocus: true,
+                          ),
+                        ),
+                        IconButton(icon: Icon(Icons.check, size: 20), onPressed: () {
+                          setState(() => isEditingName = false);
+                          updateNameInDatabase();
+                        }),
+                      ],
+                    ),
+                    Text(username, style: TextStyle(fontSize: 18)),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ProfileInfoBox(title: course, subtitle: "Subject"),
+                        GestureDetector(
+                          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => FriendsPopup(friendsDetails: friendsDetails))),
+                          child: ProfileInfoBox(title: "${friendsDetails.length} Friends", subtitle: ""),
+                        ),
+                        ProfileInfoBox(title: university, subtitle: "University"),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: isEditingBio
+                                    ? TextField(
+                                  controller: TextEditingController(text: bio),
+                                  onChanged: (value) => bio = value,
+                                  decoration: InputDecoration(hintText: "Edit your bio", border: InputBorder.none),
+                                  autofocus: true,
+                                )
+                                    : Text(bio),
+                              ),
+                              IconButton(icon: Icon(isEditingBio ? Icons.check : Icons.edit), onPressed: () {
+                                if (isEditingBio) {
+                                  final user = _auth.currentUser;
+                                  if (user != null) {
+                                    databaseReference.child('Users/${user.uid}/bio').set(bio);
+                                  }
+                                }
+                                setState(() => isEditingBio = !isEditingBio);
+                              }),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(height: 20),
-                ProfileBioBox(
-                  initialBio: "I like football",
-                  onBioChanged: (newBio) {
-                    //  bio
-                  },
-                ),
-              ],
-            ),
-          ),
-          if (isDropdownOpen)
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: FriendsPopup(
-                friends: friends,
-                onFriendSelected: (friend) {
-                  setState(() {
-                    isDropdownOpen = false;
-                  });
-                },
-                onClose: () => setState(() {
-                  isDropdownOpen = false;
-                }),
               ),
             ),
-        ],
+          ),
+        ),
       ),
     );
+
   }
 
-  BottomNavigationBarItem bottomNavItem(IconData icon, String label) {
-    return BottomNavigationBarItem(
-      icon: Icon(icon),
-      label: label,
-    );
+
+
+  String getProfilePicturePath(int profilePic) {
+    switch (profilePic) {
+      case 1: return "assets/purple.png";
+      case 2: return "assets/blue.png";
+      case 3: return "assets/blue-purple.png";
+      case 4: return "assets/orange.png";
+      case 5: return "assets/pink.png";
+      case 6: return "assets/turquoise.png";
+      default: return "assets/blue.png";
+    }
   }
 }
 
 class ProfileInfoBox extends StatelessWidget {
   final String title;
-
-  ProfileInfoBox({required this.title});
+  final String subtitle;
+  ProfileInfoBox({required this.title, required this.subtitle});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(10),
-      ),
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(10)),
       child: Column(
         children: [
           Text(title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
           SizedBox(height: 5),
-          Text("", style: TextStyle(fontSize: 14)),
-        ],
-      ),
-    );
-  }
-}
-
-class ProfileFriendsButton extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  ProfileFriendsButton({required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: onPressed,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                Text("Friends", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ProfileBioBox extends StatefulWidget {
-  final String initialBio;
-  final void Function(String newBio) onBioChanged;
-
-  ProfileBioBox({required this.initialBio, required this.onBioChanged});
-
-  @override
-  _ProfileBioBoxState createState() => _ProfileBioBoxState();
-}
-
-class _ProfileBioBoxState extends State<ProfileBioBox> {
-  late String _bio;
-  late TextEditingController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _bio = widget.initialBio;
-    _controller = TextEditingController(text: _bio);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.symmetric(horizontal: 20),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text("Bio", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-              Spacer(),
-              IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  widget.onBioChanged(_bio);
-                },
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          TextFormField(
-            controller: _controller,
-            onChanged: (value) {
-              setState(() {
-                _bio = value;
-              });
-            },
-            decoration: InputDecoration(
-              hintText: "Type your bio here...",
-              border: InputBorder.none,
-            ),
-          ),
+          Text(subtitle, style: TextStyle(fontSize: 14)),
         ],
       ),
     );

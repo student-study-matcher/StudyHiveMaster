da4bc338f1328603ca01652056f24bc6abf44012
AngelinaHdr <147830324+AngelinaHdr@users.noreply.github.com>
2024-04-28 23:21:15 +0100

    Update HomeScreen.dart

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,74 +1,171 @@
 import 'package:flutter/material.dart';
-import 'index.dart';
+import 'CustomAppBar.dart';
+import 'OpenDrawer.dart';
+import 'OpenForum.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> appForums = [];
+  List<Map<String, dynamic>> forYouForums = [];
+  List<Map<String, dynamic>> friendsForums = [];
+  List<String> userFriends = [];
+  String course = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
+
+  @override
+  void initState() {
+    super.initState();
+    getForums();
+  }
+
+  Future<void> getForums() async {
+    User? user = _auth.currentUser;
+    if (user != null) {
+      final userSnapshot = await databaseReference.child('Users/${user.uid}')
+          .get();
+      if (userSnapshot.exists) {
+        final userData = Map<String, dynamic>.from(userSnapshot.value as Map);
+        Map<dynamic, dynamic>? friendsMap = userData['friends'];
+        course = userData['course'] ?? '';
+        if (friendsMap != null) {
+          userFriends = friendsMap.keys.map((key) => key.toString()).toList();
+        }
+      }
+    }
+
+    final DatabaseReference forumsRef = FirebaseDatabase.instance.ref('Forums');
+    final forumSnapshot = await forumsRef.get();
+    if (forumSnapshot.exists) {
+      final Map<dynamic, dynamic> snapshotValue = forumSnapshot.value as Map<
+          dynamic,
+          dynamic>;
+      snapshotValue.forEach((key, value) {
+        final forum = Map<String, dynamic>.from(value);
+        forum['id'] = key;
+        setState(() {
+          appForums.add(forum);
+          if (forum["subject"] == course) {
+            forYouForums.add(forum);
+          }
+          if (userFriends.contains(forum["authorID"])) {
+            friendsForums.add(forum);
+          }
+        });
+      });
+    }
+  }
+
+  void navigateToForum(String forumId) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (_) => OpenForum(forumId: forumId)));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       drawer: OpenDrawer(),
       appBar: CustomAppBar(),
-
       body: Padding(
-        padding: EdgeInsets.all(2),
+        padding: EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            sectionTitle("Top Forums For You"),
-            sectionListTile("Software Engineering", "Computer Science"),
-            sectionListTile("BIDMAS", "Maths"),
-            sectionListTile("Bones", "Science"),
-            sectionTitle("Your Friends Most Recent Posts"),
-            sectionListTile("Big Data", "Computer Science"),
-            sectionListTile("UI Design", "Computer Science"),
-            sectionListTile("Databases", "Computer Science"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Top Forums for You",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 17,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: Card(
+                elevation: 4,
+                color: Colors.grey[100],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    itemCount: forYouForums.isEmpty ? 1 : forYouForums.length,
+                    itemBuilder: (context, index) {
+                      if (forYouForums.isEmpty) {
+                        return ListTile(
+                          title: Text('No forums available'),
+                        );
+                      } else {
+                        final item = forYouForums[index];
+                        return ListTile(
+                          title: Text(item["title"] ?? 'No Title'),
+                          subtitle: Text(item["content"] ?? 'No Content'),
+                          leading: Icon(Icons.forum),
+                          trailing: Icon(Icons.arrow_forward),
+                          onTap: () => navigateToForum(item["id"]),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20), // Adjust the height to create spacing
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Recent Posts from Friends",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 17,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: Card(
+                elevation: 4,
+                color: Colors.grey[100],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    itemCount: friendsForums.isEmpty ? 1 : friendsForums.length,
+                    itemBuilder: (context, index) {
+                      if (friendsForums.isEmpty) {
+                        return ListTile(
+                          title: Text('No recent posts from friends'),
+                        );
+                      } else {
+                        final item = friendsForums[index];
+                        return ListTile(
+                          title: Text(item["title"] ?? 'No Title'),
+                          subtitle: Text(item["content"] ?? 'No Content'),
+                          leading: Icon(Icons.forum),
+                          trailing: Icon(Icons.arrow_forward),
+                          onTap: () => navigateToForum(item["id"]),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-
-  Widget sectionTitle(String title) {
-    return Text(
-      title,
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontWeight: FontWeight.w500,
-        fontSize: 17,
-        color: Colors.black,
-      ),
-    );
-  }
-
-  Widget sectionListTile(String title, String subtitle) {
-    return ListTile(
-      tileColor: Color(0x1f000000),
-      title: Text(
-        title,
-        style: TextStyle(
-          fontWeight: FontWeight.w400,
-          fontSize: 14,
-          color: Colors.black,
-        ),
-      ),
-      subtitle: Text(
-        subtitle,
-        style: TextStyle(
-          fontWeight: FontWeight.w400,
-          fontSize: 14,
-          color: Colors.black,
-        ),
-      ),
-      dense: false,
-      contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
-      selected: false,
-      selectedTileColor: Color(0x42000000),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.zero,
-        side: BorderSide(color: Color(0x4d000000), width: 1),
-      ),
-      leading: Icon(Icons.article, color: Color(0xff212435), size: 24),
-      trailing: Icon(Icons.arrow_forward, color: Color(0xff212435), size: 24),
-    );
-  }
 }

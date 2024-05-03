5b3effe9b81408729e5e39e657d0f9c13b0aec8a
billywolfe321 <127870679+billywolfe321@users.noreply.github.com>
2024-05-03 11:59:48 +0100

    Update Registration1.dart

--- lib/Registration1.dart
+++ lib/Registration1.dart
@@ -37,13 +37,16 @@ class Registration1State extends State<Registration1> {
         print("registration successful");
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Registration2(user: newUser.user)),
+          MaterialPageRoute(
+              builder: (context) => Registration2(user: newUser.user)),
         );
       }
     } on FirebaseAuthException catch (e) {
-      _showDialog('Registration Error', e.message ?? 'An unknown error occurred.');
+      _showDialog(
+          'Registration Error', e.message ?? 'An unknown error occurred.');
     }
   }
+
   void _showDialog(String title, String message) {
     showDialog(
       context: context,
@@ -55,52 +58,147 @@ class Registration1State extends State<Registration1> {
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registration'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(labelText: 'Email'),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
+          width: MediaQuery
+              .of(context)
+              .size
+              .width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF8A2387),
+                Color(0xFFE94057),
+                Color(0xFFF27121),
+              ],
             ),
-            SizedBox(height: 8.0),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            SizedBox(height: 8.0),
-            TextField(
-              controller: confirmPasswordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Confirm Password'),
-            ),
-            SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: _registerUser,
-              child: Text('Register'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => Login()),
-                );
-              },
-              child: Text('Already have an account? Login'),
-            ),
-          ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 80),
+              Image.asset(
+                'assets/logo.png',
+                width: 150,
+                height: 50,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Study Hive',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+
+              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.all(20),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width * 0.5,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Registration',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: confirmPasswordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: 'Confirm Password'),
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: _registerUser,
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFF8A2387),
+                              Color(0xFFE94057),
+                              Color(0xFFF27121),
+                            ],
+                          ),
+                        ),
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) =>
+                              Registration2()),
+                        );
+                      },
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                        child: Text('Already have an account? Login'),
+                      ),
+                    ),
+                  ],
+
+                ),
+              ),
+
+            ],
+          ),
         ),
       ),
+
     );
   }
 }
